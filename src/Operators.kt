57ecd6f4e7a2536e68904cacd70f7de52667443a
darshitdudhaiya <darshitdudhaiya201@gmail.com>
2025-03-22 03:55:41 +0530

    Variables and Operators

---
+++ src/Operators.kt
@@ -0,0 +1,26 @@
+fun main() {
+    var i: Int = 10
+    var j: Int = 15
+
+    // Arithmetic Operators
+    println(i + j)
+    println(i - j)
+    println(i * j)
+    println(i.toFloat() / j)
+
+    // Relational Operators
+    println(i > j)
+    println(i < j)
+    println(i >= j)
+    println(i <= j)
+    println(i == j)
+    println(i != j)
+
+    // Increment / Decrement Operators
+//    i++
+    println(i)
+    --j
+    println(j)
+
+    println(i++ + ++i)
+}
